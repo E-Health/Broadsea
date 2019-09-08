5bcdf8b96c18639c3786b3e3ed86240c66344846
dermatologist <github@gulfdoctor.net>
2019-09-08 08:32:52 -0400

    source-daimon with multiple sources

--- postgresql/source_source_daimon.sql
+++ postgresql/source_source_daimon.sql
@@ -5,7 +5,7 @@ truncate ohdsi.source cascade;
 -- OHDSI CDM source
 INSERT INTO ohdsi.source( source_id, source_name, source_key, source_connection, source_dialect)
 VALUES (1, 'Synpuf-5p', 'OHDSI-CDMV5',
-  'jdbc:postgresql://192.168.0.251:5432/beapen_db?user=beapen', 'postgresql');
+  'jdbc:postgresql://192.168.0.251:5432/<user>_db?user=<user>', 'postgresql');
 
 -- CDM daimon
 INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table_qualifier, priority) VALUES (1, 1, 0, 'synpuf5', 2);
@@ -18,3 +18,22 @@ INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table
 
 -- EVIDENCE daimon
 INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table_qualifier, priority) VALUES (4, 1, 3, 'temp', 2);
+
+
+
+-- OHDSI CDM source
+INSERT INTO ohdsi.source( source_id, source_name, source_key, source_connection, source_dialect)
+VALUES (2, 'Discharge Abstract Database', 'DAD-CDMV5',
+  'jdbc:postgresql://192.168.0.251:5432/<user>_db?user=<user>', 'postgresql');
+
+-- CDM daimon
+INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table_qualifier, priority) VALUES (5, 2, 0, 'dad', 2);
+
+-- VOCABULARY daimon
+INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table_qualifier, priority) VALUES (6, 2, 1, 'vocabulary', 2);
+
+-- RESULTS daimon
+INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table_qualifier, priority) VALUES (7, 2, 2, 'results', 2);
+
+-- EVIDENCE daimon
+INSERT INTO ohdsi.source_daimon( source_daimon_id, source_id, daimon_type, table_qualifier, priority) VALUES (8, 2, 3, 'temp', 2);
